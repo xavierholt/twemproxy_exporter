359ab7b43409d3f04d6c30a5d07e027bd44e0110
Andrew Tongen <atongen@gmail.com>
2017-02-13 19:32:09 -0600

    Add initial spec

---
+++ spec/twemproxy_exporter/counter_spec.rb
@@ -0,0 +1,30 @@
+require "spec_helper"
+
+describe TwemproxyExporter::Counter do
+  before(:all) do
+    registry = Prometheus::Client.registry
+    @counter = TwemproxyExporter::Counter.new(registry, :test_total, "A test counter")
+  end
+
+  context "#count" do
+    it "should only increase" do
+      expect do
+        @counter.count(5)
+      end.to change { @counter.send(:instance_variable_get, :@counter).get }.by(5)
+
+      expect(@counter.send(:instance_variable_get, :@last)).to equal(5)
+
+      expect do
+        @counter.count(4)
+      end.to change { @counter.send(:instance_variable_get, :@counter).get }.by(4)
+
+      expect(@counter.send(:instance_variable_get, :@last)).to equal(4)
+
+      expect do
+        @counter.count(6)
+      end.to change { @counter.send(:instance_variable_get, :@counter).get }.by(2)
+
+      expect(@counter.send(:instance_variable_get, :@last)).to equal(6)
+    end
+  end
+end
